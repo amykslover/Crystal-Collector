2a80dbf2e15f24afb82de58949fc1158c5c5f590
amykslover <amykslover@gmail.com>
2017-10-16 12:48:44 -0700

    Adding goodbye page and separating JS into new file

---
+++ assets/js/game.js
@@ -0,0 +1,118 @@
+var masterNumber = Math.floor(Math.random() * 80);
+var crystal1 = Math.floor(Math.random() * 10)+1;
+var crystal2 = Math.floor(Math.random() * 10)+1;
+var crystal3 = Math.floor(Math.random() * 10)+1;
+var crystal4 = Math.floor(Math.random() * 10)+1;
+var crystal5 = Math.floor(Math.random() * 10)+1;
+var countWins = 0;
+var countLoss = 0;
+var guessTotal = 0;
+
+
+console.log(crystal1);
+console.log(crystal2);
+console.log(crystal3);
+console.log(crystal4);
+console.log(crystal5);
+console.log(masterNumber);
+
+function checkStatus() {
+
+    if (guessTotal == masterNumber) {
+        setTimeout(function(){
+            alert("You won!");
+            countWins = countWins + 1;
+            $("#wins").text("W: " + countWins);
+            var result = confirm("Do you want to play again?");
+            if(result) {
+                resetGame();
+            } else {
+                $(location).attr('href', '../goodbye.html');
+            }
+        } , 100)
+        }    
+
+    if (guessTotal > masterNumber) {
+        setTimeout(function(){
+            alert("You lost!");
+            countLoss = countLoss + 1;
+            console.log("Losses: " + countLoss);
+            $("#losses").text("L: " + countLoss);
+            var result = confirm("Do you want to play again?");
+            if(result) {
+                resetGame();
+            } else {
+                $(location).attr('href', '../goodbye.html');
+            }
+   
+        } , 100)
+    }
+}
+
+function resetGame() {
+
+masterNumber = Math.floor(Math.random() * 80);
+crystal1 = Math.floor(Math.random() * 10)+1;
+crystal2 = Math.floor(Math.random() * 10)+1;
+crystal3 = Math.floor(Math.random() * 10)+1;
+crystal4 = Math.floor(Math.random() * 10)+1;
+crystal5 = Math.floor(Math.random() * 10)+1;
+guessTotal = 0;
+$("#result").text(guessTotal);
+$("#numberGuess").text(masterNumber);
+console.log(crystal1);
+console.log(crystal2);
+console.log(crystal3);
+console.log(crystal4);
+console.log(crystal5);
+console.log(masterNumber);
+};
+
+
+$(document).ready(function(){
+
+
+$("#numberGuess").text(masterNumber);
+
+$("#result").text(guessTotal);
+
+    if(guessTotal < masterNumber) {
+
+        $("#gem1").click(function(){
+            console.log("Console log:" + crystal1);
+            guessTotal = guessTotal + crystal1;
+            $("#result").text(guessTotal);
+            checkStatus();
+        });
+
+        $("#gem2").click(function(){
+            console.log("Console log:" + crystal2);
+            guessTotal = guessTotal + crystal2;
+            $("#result").text(guessTotal);
+            checkStatus();
+
+        });
+
+        $("#gem3").click(function(){
+            console.log("Console log:" + crystal3);
+            guessTotal = guessTotal + crystal3;
+            $("#result").text(guessTotal); 
+            checkStatus();
+        });
+
+        $("#gem4").click(function(){
+            console.log("Console log:" + crystal4);
+            guessTotal = guessTotal + crystal4;
+            $("#result").text(guessTotal);
+            checkStatus();
+        });
+
+        $("#gem5").click(function(){
+            console.log("Console log:" + crystal4);
+            guessTotal = guessTotal + crystal5;
+            $("#result").text(guessTotal);
+            checkStatus();
+        });
+
+        } 
+});
